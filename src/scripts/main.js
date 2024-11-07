9a3dde88cd349e68f5f68c9b70ae2164cb7b6f29
Pasha Shchukin <pasha4320692@gmail.com>
2024-11-07 21:10:21 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve('Promise was resolved!');
+  });
+});
+
+// eslint-disable-next-line promise/param-names, no-unused-vars
+const promise2 = new Promise((_, reject) => {
+  setTimeout(() => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject('Promise was rejected!');
+  }, 3000);
+});
+
+function addMessage(text, isError = false) {
+  const messageDiv = document.createElement('div');
+
+  messageDiv.className = 'message';
+
+  if (isError) {
+    messageDiv.classList.add('error-message');
+  }
+  messageDiv.textContent = text;
+  document.body.appendChild(messageDiv);
+}
+
+promise1
+  .then((message) => {
+    addMessage(message);
+  })
+  .catch((error) => {
+    addMessage(error, true);
+  });
+
+promise2
+  .then((message) => {
+    addMessage(message);
+  })
+  .catch((error) => {
+    addMessage(error, true);
+  });
